190a9aef7ae18ccccdfdbf7874bf0294b217c373
Hannah McCullough <hmac091@gmail.com>
2022-01-05 13:27:20 -0700

    added toggle for details

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
+import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
 
 function BeerCard({ beer, handleVoteClick }) {
+    const [detailsToggle, setDetailsToggle] = useState(false);
     
     const { id, image, name, brewery, category, votes, notes, region, abv } = beer;
 
-    function handleImgClick() {
-        console.log('Imageclicked');
-    }
 
     return (
         <div className="container">
             <div className="card">
-                <img src={image} alt={name} onClick={handleImgClick} />
+                <img src={image} alt={name} />
                     <div className='beer-name'>
                         <h4>{name} <br/> {brewery}</h4>
                     </div>
-                <div className='card-details'>
+                <div className='card-btns'>
+                    <button style={{float: 'left'}}
+                        className="card-button" 
+                        id={id} vote={votes} 
+                        onClick={() => handleVoteClick(id, votes)}
+                    >Vote</button>
+                    <button style={{float: 'right'}}
+                        className="card-button" 
+                        id={id} vote={votes} 
+                        onClick={() => setDetailsToggle(!detailsToggle)}
+                    >{detailsToggle ? <VscChevronUp/> : <VscChevronDown/>}</button>
+                </div>
+                <div style={{display: detailsToggle ? 'block' : 'none'}}className='card-details'>
+                    <h4 style={{textDecoration: 'overline'}}>Specifications</h4>
                     <p>Category: {category} </p>
                     <p>Region: {region}</p>
                     <p>ABV: {abv}%</p>
                     <p>Votes: {votes}</p>
-                    <button className="button" id={id} vote={votes} onClick={() => handleVoteClick(id, votes)}>It's got my vote!</button>
                 </div>
             </div>
         </div>
