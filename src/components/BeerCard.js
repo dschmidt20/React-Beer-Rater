94526d898a81f88583e6811f1200ad6167c1dd99
Hannah McCullough <hmac091@gmail.com>
2022-01-05 11:44:14 -0700

    added POST for vote click

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,30 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function BeerCard({ beer }) {
-
-    const { image, name, category, votes, notes, region, abv } = beer;
+function BeerCard({ beer, handleVoteClick }) {
+    
+    const { id, image, name, brewery, category, votes, notes, region, abv } = beer;
 
     function handleImgClick() {
         console.log('Imageclicked');
-    }
-
-    function handleBtnClick() {
-        console.log('Button clicked')
     }
 
     return (
         <div className="container">
             <div className="card">
                 <img src={image} alt={name} onClick={handleImgClick} />
-                <div className='card-details'>
                     <div className='beer-name'>
-                        <h4>{name}</h4>
+                        <h4>{name} <br/> {brewery}</h4>
                     </div>
+                <div className='card-details'>
                     <p>Category: {category} </p>
                     <p>Region: {region}</p>
                     <p>ABV: {abv}%</p>
                     <p>Votes: {votes}</p>
-                    <button className="button" onClick={handleBtnClick}>It's got my vote!</button>
+                    <button className="button" id={id} vote={votes} onClick={() => handleVoteClick(id, votes)}>It's got my vote!</button>
                 </div>
             </div>
         </div>
